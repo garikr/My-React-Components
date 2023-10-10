0f87cd1c73c7d4aa63bf055c5d35489a5af8e91e
Ihor Rybalko <garik.rybalko@gmail.com>
2023-10-10 15:21:03 +0300

    Added Accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,27 @@
-import { GoBell, GoDownload, GoDatabase } from "react-icons/go";
-import Button from "./button";
+import Accordion from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log("Click!!");
-  };
-  return (
-    <div>
-      <div>
-        <Button
-          secondary
-          outline
-          rounded
-          className="mb-5"
-          onClick={handleClick}
-        >
-          <GoBell />
-          Click Here!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline onMouseEnter={handleClick}>
-          <GoDownload />
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <GoDatabase />
-          Sea Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button primary rounded>
-          Something!
-        </Button>
-      </div>
-    </div>
-  );
+  const items = [
+    {
+      id: "0",
+      label: "Can I use React on a project?",
+      content:
+        "Yes, you can use React on a project. React is a JavaScript library for building user interfaces. It is declarative, efficient, and flexible. You can use React to build anything from simple websites to complex web applications.",
+    },
+    {
+      id: "1",
+      label: "Can I use Javascript on a project?",
+      content:
+        "Yes, you can use JavaScript on a project. JavaScript is a programming language that is used to add interactivity and functionality to web pages. It is also used to develop server-side applications, mobile apps, and desktop apps.",
+    },
+    {
+      id: "2",
+      label: "Can I use CSS on a project?",
+      content:
+        "Yes, you can use CSS on a project. CSS is a cascading style sheet language that is used to style HTML elements. It can be used to control the look and feel of your web pages, such as the font, color, and layout.",
+    },
+  ];
+  return <Accordion items={items} />;
 }
 
 export default App;
