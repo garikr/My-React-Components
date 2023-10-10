7470f2668e482e18cb27cbf285ddfed8f0ca8959
Ihor Rybalko <garik.rybalko@gmail.com>
2023-10-10 20:35:30 +0300

    Dropdown and Panel components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
-import Accordion from "./components/Accordion";
+import { useState } from "react";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  const items = [
-    {
-      id: "0",
-      label: "Can I use React on a project?",
-      content:
-        "Yes, you can use React on a project. React is a JavaScript library for building user interfaces. It is declarative, efficient, and flexible. You can use React to build anything from simple websites to complex web applications.",
-    },
-    {
-      id: "1",
-      label: "Can I use Javascript on a project?",
-      content:
-        "Yes, you can use JavaScript on a project. JavaScript is a programming language that is used to add interactivity and functionality to web pages. It is also used to develop server-side applications, mobile apps, and desktop apps.",
-    },
-    {
-      id: "2",
-      label: "Can I use CSS on a project?",
-      content:
-        "Yes, you can use CSS on a project. CSS is a cascading style sheet language that is used to style HTML elements. It can be used to control the look and feel of your web pages, such as the font, color, and layout.",
-    },
+  const [selection, setSelection] = useState(null);
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
   ];
-  return <Accordion items={items} />;
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AccordionPage.jsx
@@ -0,0 +1,27 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+  const items = [
+    {
+      id: "0",
+      label: "Can I use React on a project?",
+      content:
+        "Yes, you can use React on a project. React is a JavaScript library for building user interfaces. It is declarative, efficient, and flexible. You can use React to build anything from simple websites to complex web applications.",
+    },
+    {
+      id: "1",
+      label: "Can I use Javascript on a project?",
+      content:
+        "Yes, you can use JavaScript on a project. JavaScript is a programming language that is used to add interactivity and functionality to web pages. It is also used to develop server-side applications, mobile apps, and desktop apps.",
+    },
+    {
+      id: "2",
+      label: "Can I use CSS on a project?",
+      content:
+        "Yes, you can use CSS on a project. CSS is a cascading style sheet language that is used to style HTML elements. It can be used to control the look and feel of your web pages, such as the font, color, and layout.",
+    },
+  ];
+  return <Accordion items={items} />;
+}
+
+export default AccordionPage;
